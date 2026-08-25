85bb43e2d4262b505449a4dd56e1793171da4346
Mohammed Taha <192572173.simats@saveetha.com>
2026-08-25 08:08:41 +0530

    Allow Netlify frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -57,7 +57,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "https://beautiful-valkyrie-2a7bed.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
